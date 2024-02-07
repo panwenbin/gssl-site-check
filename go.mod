82daf205683134f2478a98445a6e2f5c5412dfee
panwenbin <panwenbin@gmail.com>
2024-02-07 10:34:59 +0800

    ssl-dates

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/panwenbin/gssl-site-check
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type SSLInfo struct {
+	Website   string    `json:"website"`
+	NotBefore time.Time `json:"not_before"`
+	NotAfter  time.Time `json:"not_after"`
+	IdValid   bool      `json:"is_valid"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func getSSLCertificateDates(hostname string) (time.Time, time.Time, error) {
+	conn, err := tls.Dial("tcp", hostname+":443", nil)
+	if err != nil {
+		return time.Time{}, time.Time{}, err
+	}
+	defer conn.Close()
+
+	state := conn.ConnectionState()
+	for _, cert := range state.PeerCertificates {
+		return cert.NotBefore, cert.NotAfter, nil
+	}
+
+	return time.Time{}, time.Time{}, fmt.Errorf("unable to retrieve SSL certificate information")
+}
+
+func sslInfoHandler(w http.ResponseWriter, r *http.Request) {
+	website := r.URL.Query().Get("website")
+	if website == "" {
+		errorResponse := ErrorResponse{Error: "Missing 'website' parameter"}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	notBefore, notAfter, err := getSSLCertificateDates(website)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	sslInfo := SSLInfo{
+		Website:   website,
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+		IdValid:   time.Now().Before(notAfter),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(sslInfo)
+}
+
+func main() {
+	http.HandleFunc("/ssl-dates", sslInfoHandler)
+
+	port := 8080
+	fmt.Printf("Server is running on :%d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
