371c9f7d77c9c087739b1ce688f522f2ef87537a
panwenbin <panwenbin@gmail.com>
2024-02-07 14:43:16 +0800

    ssl-dates and ssl-chain

--- main.go
+++ main.go
@@ -43,7 +43,46 @@ func getSSLCertificate(hostname string) (*x509.Certificate, error) {
 	return nil, fmt.Errorf("unable to retrieve SSL certificate information")
 }
 
+func getSSLCertificateChain(hostname string) ([]*x509.Certificate, error) {
+	dialer := &net.Dialer{
+		Timeout:   2 * time.Second,
+		DualStack: true,
+	}
+
+	conn, err := tls.DialWithDialer(dialer, "tcp", hostname+":443", &tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	state := conn.ConnectionState()
+	return state.PeerCertificates, nil
+}
+
 func sslInfoHandler(w http.ResponseWriter, r *http.Request) {
+	website := r.URL.Query().Get("website")
+	if website == "" {
+		errorResponse := ErrorResponse{Error: "Missing 'website' parameter"}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	cert, err := getSSLCertificate(website)
+	if err != nil {
+		errorResponse := ErrorResponse{Error: err.Error()}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cert)
+}
+
+func sslDatesHandler(w http.ResponseWriter, r *http.Request) {
 	website := r.URL.Query().Get("website")
 	if website == "" {
 		errorResponse := ErrorResponse{Error: "Missing 'website' parameter"}
@@ -75,8 +114,33 @@ func sslInfoHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(sslInfo)
 }
 
+func sslChainHandler(w http.ResponseWriter, r *http.Request) {
+	website := r.URL.Query().Get("website")
+	if website == "" {
+		errorResponse := ErrorResponse{Error: "Missing 'website' parameter"}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	certs, err := getSSLCertificateChain(website)
+	if err != nil {
+		errorResponse := ErrorResponse{Error: err.Error()}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(certs)
+}
+
 func main() {
-	http.HandleFunc("/ssl-dates", sslInfoHandler)
+	http.HandleFunc("/ssl-info", sslInfoHandler)
+	http.HandleFunc("/ssl-dates", sslDatesHandler)
+	http.HandleFunc("/ssl-chain", sslChainHandler)
 
 	port := 8080
 	fmt.Printf("Server is running on :%d...\n", port)
